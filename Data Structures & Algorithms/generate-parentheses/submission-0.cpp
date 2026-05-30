43420a206c18b7b5f46df2625d976f2a59fc3fa0
Guna Lankalapalli <lankalapalligunapersonal@gmail.com>
2026-05-30 22:54:10 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    void helper(int op,int cl,string curr,vector<string>& res,int n)
+    {
+        //bc
+        if(op == n && cl == n)
+        {
+            res.push_back(curr);
+            return;
+        }
+
+        //pick (
+        if(op < n)
+        {
+            helper(op + 1,cl,curr + "(",res,n);
+        }
+
+        //pick )
+        if(op > cl)
+        {
+            helper(op,cl + 1,curr + ")",res,n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        //logic is just to use helper func
+        //2 vars for tracking open,close bracks
+        //inc them if putting into curr
+        //push into res if op , cl are n
+        //do not put ) if op <= cl
+        //use pick,not pick like recursion but ( or )
+
+        vector<string> res;
+        string curr = "";
+
+        helper(0,0,curr,res,n);
+
+        return res;
+    }
+};
