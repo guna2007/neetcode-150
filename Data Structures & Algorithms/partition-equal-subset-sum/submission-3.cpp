9406d603d9f32c83918e3b9f8a6f398863fda6f7
Guna Lankalapalli <lankalapalligunapersonal@gmail.com>
2026-08-12 19:04:36 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum %2 != 0) return false;
+        int target = sum/2;
+
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+
+        //for t = 0
+        for(int i = 0;i <= n;i++)
+        {
+            dp[i][0] = 1;
+        }
+
+        for(int i = n-1;i >= 0;i--)
+        {
+            for(int j = 1;j <= target;j++)
+            {
+                bool res = 0;
+                if(nums[i] <= j)
+                {
+                    res = res | dp[i+1][j-nums[i]];
+                }
+                res = res | dp[i+1][j];
+
+                dp[i][j] = res;
+            }
+        }
+
+        return dp[0][target];
+
+      
+    }
+};
